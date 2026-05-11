69b9d83e42952ee020cd77b7720c80481645c187
Brenda Oliveira <brendabo3001@gmail.com>
2026-05-11 20:00:10 -0300

    lucas maravilhoso

---
+++ src/test/java/com/example/kontrol/KontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
